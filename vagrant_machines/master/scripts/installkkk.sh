3b12c33e305919207b4528f1a0d44bc7cefb1ed6
Alejandro León <alejandroleon09@gmail.com>
2018-08-22 22:06:45 -0500

    Installation using Calico CNI

--- vagrant_machines/master/scripts/installkkk.sh
+++ vagrant_machines/master/scripts/installkkk.sh
@@ -8,7 +8,18 @@ KUBE_VERSION=1.11.2-00
 apt-get update
 sudo apt-get -y install docker-ce=17.03.2~ce-0~ubuntu-xenial
 apt-get install -y --allow-downgrades kubelet=$KUBE_VERSION kubeadm=$KUBE_VERSION kubectl=$KUBE_VERSION
-wget -O kube-flannel.yml https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
-FLANNEL_NETWORK=$(grep "Network.*/16" kube-flannel.yml|nawk '{print substr($2,2,length($2)-3)}')
-kubeadm init --pod-network-cidr $FLANNEL_NETWORK 2>&1 kubeadm.log
+
+function install_with_CNI_flanel() {
+  wget -O kube-flannel.yml https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
+  FLANNEL_NETWORK=$(grep "Network.*/16" kube-flannel.yml|nawk '{print substr($2,2,length($2)-3)}')
+  kubeadm init --pod-network-cidr $FLANNEL_NETWORK 2>&1 kubeadm.log
+}
+
+function install_with_CNI_calico() {
+  wget -O calico.yml  https://docs.projectcalico.org/v2.6/getting-started/kubernetes/installation/hosted/kubeadm/1.6/calico.yaml
+  CALICO_NETWORK=$(grep "value.*/16" calico.yml|nawk '{print substr($2,2,length($2)-2)}')
+  kubeadm init --pod-network-cidr $CALICO_NETWORK 2>&1 kubeadm.log
+}
+
+install_with_CNI_calico
 grep -i token kubeadm.log
