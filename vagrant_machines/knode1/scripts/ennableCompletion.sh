4ae96352d433b8977e7dbb702a8d2ac3a3efc8cc
Alejandro León <alejandroleon09@gmail.com>
2018-09-19 00:51:03 -0500

    Maternode and tutorial updated

---
+++ vagrant_machines/knode1/scripts/ennableCompletion.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+#Ennable command completion
+echo "source <(kubectl completion bash)"|sudo tee -a /etc/bash.bashrc
+echo "source <(kubeadm completion bash)"|sudo tee -a /etc/bash.bashrc
