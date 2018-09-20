82d09412b3aa2abfaf96ff52fc01b7637dedeb7b
Alejandro León <alejandroleon09@gmail.com>
2018-09-19 23:16:16 -0500

    ETH1 IP added to k8s minion

--- vagrant_machines/knode1/scripts/ennableCompletion.sh
+++ vagrant_machines/knode1/scripts/ennableCompletion.sh
@@ -3,3 +3,5 @@
 echo "source <(kubectl completion bash)"|sudo tee -a /etc/bash.bashrc
 echo "source <(kubeadm completion bash)"|sudo tee -a /etc/bash.bashrc
 #Fixing to secondary network
+LOCAL_IP=$1
+sudo sed -i 's/KUBELET_EXTRA_ARGS=.*/KUBELET_EXTRA_ARGS=--node-ip='$LOCAL_IP'/' /etc/default/kubelet
