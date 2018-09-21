072319423cf2bbabaccd9b1c58194f384fa64bb1
Alejandro León <alejandroleon09@gmail.com>
2018-09-20 23:24:31 -0500

    Fix kubectl to point eht1 in the vMs

--- vagrant_machines/knode1/scripts/noRootUserAccess.sh
+++ vagrant_machines/knode1/scripts/noRootUserAccess.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 mkdir ~vagrant/.kube
-sudo cp -i /etc/kubernetes/admin.conf ~vagrant/.kube/config
 sudo chown -R $(id -u vagrant):$(id -g vagrant) ~vagrant/.kube
--- vagrant_machines/master/scripts/installkkk.sh
+++ vagrant_machines/master/scripts/installkkk.sh
@@ -5,10 +5,16 @@ cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
 deb http://apt.kubernetes.io/ kubernetes-xenial main
 EOF
 #KUBE_VERSION=1.10.1-00
-KUBE_VERSION=1.11.2-00
+KUBE_VERSION=1.11.3-00
 apt-get update
 sudo apt-get -y install docker-ce=17.03.2~ce-0~ubuntu-xenial
 sudo apt-get install -y --allow-downgrades kubelet=$KUBE_VERSION kubeadm=$KUBE_VERSION kubectl=$KUBE_VERSION
+#Configuring kubectl to the secondary vm network
+echo "secondary IP vagrant: $1"
+LOCAL_IP=$1
+sudo sed -i 's/KUBELET_EXTRA_ARGS=.*/KUBELET_EXTRA_ARGS=--node-ip='$LOCAL_IP'/' /etc/default/kubelet
+systemctl daemon-reload
+systemctl restart kubelet
 
 function install_with_CNI_flanel () {
   wget -O kube-flannel.yml https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
@@ -17,13 +23,11 @@ function install_with_CNI_flanel () {
 }
 
 function install_with_CNI_calico () {
-  #wget -O calico.yml  https://docs.projectcalico.org/v2.6/getting-started/kubernetes/installation/hosted/kubeadm/1.6/calico.yaml
   wget -O /tmp/calico.yml  https://docs.projectcalico.org/v3.2/getting-started/kubernetes/installation/hosted/calico.yaml
   CALICO_NETWORK=$(grep "value.*/16" calico.yml|nawk '{print substr($2,2,length($2)-2)}')
   echo "Calico Network to use $CALICO_NETWORK"
   pwd
-  echo "1: $1"
-  kubeadm init --apiserver-advertise-address $1 --pod-network-cidr $CALICO_NETWORK 2>&1 > kubeadm.log
+  kubeadm init --apiserver-advertise-address $LOCAL_IP --pod-network-cidr $CALICO_NETWORK 2>&1 > kubeadm.log
 }
 
 install_with_CNI_calico $1
